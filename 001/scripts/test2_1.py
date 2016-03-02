20680f8986000fffe3f169106c0e1add5100a51e
aldwinaldwin <aldwinaldwin@users.noreply.github.com>
2016-03-02 03:15:59 +0000

    test python+python+redis+python+mysql+mongodb

---
+++ 001/scripts/test2_1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import web
+import redis
+
+r_server = redis.Redis('127.0.0.1')
+
+urls = (
+    '/pushonqueue', 'pushonqueue'
+)
+
+print globals()
+
+app = web.application(urls, globals())
+
+class pushonqueue:
+    def POST(self):
+        i = web.input()
+        r_server.rpush(i.queuename,i.json)
+#        print i.queuename+' '+i.json
+        return 'OK'
+
+if __name__ == "__main__":
+    app.run()
